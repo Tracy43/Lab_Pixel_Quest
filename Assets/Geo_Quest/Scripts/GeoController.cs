9ddef2e9b83d8e21c2176cf126fb2f7e250150ce
Tracy43 <tracyambrosio@gmail.com>
2026-02-21 16:22:06 -0500

    assignment 2

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using UnityEngine;
 
 public class GeoController : MonoBehaviour
 {
     string one = "Hello";
+    int var = 0;
     // Start is called before the first frame update
     void Start()
-    {
+    { 
         Debug.Log("Hello World");
         string two = "World";
     }
@@ -15,6 +17,10 @@ public class GeoController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Hello" + "World");
+        if (Input.GetKeyDown(KeyCode.W))
+        { transform.position += new Vector3(0, 1, 0); }
+        var++;
+        Debug.Log(var);
+
     }
 }
